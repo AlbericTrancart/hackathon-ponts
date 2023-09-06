9f2aaba97fb588ff665dce965e72781025595f3d
Filomène Roquefort <filomene.roquefort@gmail.com>
2023-09-06 16:35:47 +0200

    corrections pyhton

--- ask_question_to_pdf.py
+++ ask_question_to_pdf.py
@@ -83,44 +83,85 @@ document = read_pdf(filename)
 chunks = split_text(document)
 
 
-import openai
-
 def gpt3_completion(question):
     reponse = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": question}
-        ]
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": question}]
     )
-    return reponse['choices'][0]['message']['content']
+    return reponse["choices"][0]["message"]["content"]
 
 
-#print(gpt3_completion('quel jour on est?'))
+la_question = "qui es-tu?"
 
-text = "Les voitures électriques offrent plusieurs avantages par rapport aux voitures à moteur thermique : 1. **Zéro émissions locales :** Les voitures électriques ne produisent pas d'émissions d'échappement locales, réduisant ainsi la pollution de l'air et les effets sur la santé. 2. **Moins de dépendance aux combustibles fossiles :** Les voitures électriques utilisent l'électricité, qui peut provenir de sources renouvelables comme le soleil et le vent, réduisant la dépendance aux combustibles fossiles.3. **Coûts de fonctionnement réduits :** Les voitures électriques ont moins de pièces mobiles et nécessitent moins d'entretien par rapport aux voitures à moteur thermique, ce qui peut réduire les coûts à long terme. 4. **Performance instantanée :** Les voitures électriques offrent un couple élevé dès le départ, ce qui signifie une accélération rapide et fluide sans la nécessité de changer de vitesses. 5. **Conduite silencieuse :** Les moteurs électriques sont beaucoup plus silencieux que les moteurs thermiques, offrant une expérience de conduite plus paisible. 6. **Amélioration de l'efficacité énergétique :** Les voitures électriques convertissent plus efficacement l'énergie électrique en mouvement par rapport aux moteurs à combustion interne. 7. **Réduction des émissions de gaz à effet de serre :** Même en tenant compte de l'émission de CO2 liée à la production d'électricité, les voitures électriques ont tendance à produire moins d'émissions de gaz à effet de serre sur leur cycle de vie par rapport aux voitures à essence. 8. **Innovation technologique :** Les voitures électriques stimulent le développement de nouvelles technologies, telles que les batteries plus performantes et les systèmes de recharge avancés. 9. **Réduction du bruit urbain :** La diminution du bruit des véhicules électriques contribue à réduire le niveau de bruit dans les zones urbaines. 10. **Subventions et incitations :** Dans de nombreux endroits, les voitures électriques bénéficient d'incitations gouvernementales, telles que des réductions fiscales ou des voies réservées. Il est important de noter que la transition vers les voitures électriques implique également des défis, tels que l'infrastructure de recharge en expansion, la gestion des matériaux des batteries et l'autonomie limitée par rapport aux voitures à essence sur de longs trajets. Cependant, les avantages en matière d'environnement et d'efficacité continuent de renforcer l'attrait des voitures électriques pour l'avenir de la mobilité."
+
+# text = "Les voitures électriques offrent plusieurs avantages par rapport
+# aux voitures à moteur thermique :
+# 1. **Zéro émissions locales : ** Les voitures électriques ne produisent pas d'émissions d'échappement locales,
+# réduisant ainsi la pollution de l'air et les effets sur la santé.
+# 2. **Moins de dépendance aux combustibles fossiles :** Les voitures électriques utilisent l'électricité,
+# qui peut provenir de sources renouvelables comme le soleil et le vent,
+# réduisant la dépendance aux combustibles fossiles.
+# 3. **Coûts de fonctionnement réduits :
+# ** Les voitures électriques ont moins de pièces mobiles et nécessitent moins d'entretien par rapport aux voitures à moteur thermique,
+# ce qui peut réduire les coûts à long terme.
+# 4. **Performance instantanée :** Les voitures électriques offrent un couple élevé dès le départ,
+# ce qui signifie une accélération rapide et fluide sans la nécessité de changer de vitesses.
+# 5. **Conduite silencieuse :** Les moteurs électriques sont beaucoup plus silencieux que les moteurs thermiques,
+# offrant une expérience de conduite plus paisible.
+# 6. **Amélioration de l'efficacité énergétique :** Les voitures électriques convertissent plus efficacement
+# l'énergie électrique en mouvement par rapport aux moteurs à combustion interne.
+# 7. **Réduction des émissions de gaz à effet de serre :** Même en tenant compte de l'émission de CO2 liée à la production d'électricité,
+# les voitures électriques ont tendance à produire moins d'émissions de gaz à effet de serre sur leur cycle de vie
+# par rapport aux voitures à essence.
+# 8. **Innovation technologique :** Les voitures électriques stimulent le développement de nouvelles technologies,
+# telles que les batteries plus performantes et les systèmes de recharge avancés.
+# 9. **Réduction du bruit urbain :** La diminution du bruit des véhicules électriques contribue à réduire le niveau de bruit dans les zones urbaines.
+# 10. **Subventions et incitations :** Dans de nombreux endroits, les voitures électriques bénéficient d'incitations gouvernementales,
+# telles que des réductions fiscales ou des voies réservées.
+# Il est important de noter que la transition vers les voitures électriques implique également des défis,
+# tels que l'infrastructure de recharge en expansion, la gestion des matériaux des batteries
+# et l'autonomie limitée par rapport aux voitures à essence sur de longs trajets.
+# Cependant, les avantages en matière d'environnement et d'efficacité continuent de renforcer l'attrait des voitures électriques
+# pour l'avenir de la mobilité."
+
 
 def ask_question_to_pdf(question):
-    return(gpt3_completion(question + document + "Tu répondras à cette question en t'appuyant uniquement sur le texte précédent."))
+    return gpt3_completion(
+        question
+        + document
+        + "Tu répondras à cette question en t'appuyant uniquement sur le texte précédent."
+    )
 
-#print(ask_question_to_pdf('Que penses-tu des voitures électriques?'))
 
-la_question = 'qui es-tu?'
+# print(ask_question_to_pdf('Que penses-tu des voitures électriques?'))
+
 
 def gpt3_question():
     question_cree = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": document + "Pose-moi une question sur ce texte."}
-        ]
+            {
+                "role": "system",
+                "content": document + "Pose-moi une question sur ce texte.",
+            }
+        ],
     )
     la_question = question_cree
-    return question_cree['choices'][0]['message']['content']
+    return question_cree["choices"][0]["message"]["content"]
+
 
 def gpt3_answer(reponse):
     vrai_ou_faux = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": document + "en te basant uniquement sur ce texte, est-ce que" + reponse + "répond à la question" + la_question + "? Tu répondras vrai si la réponse est vraie et faux si elle est fausse, en argumentant."}
-        ]
+            {
+                "role": "system",
+                "content": document
+                + "en te basant uniquement sur ce texte, est-ce que"
+                + reponse
+                + "répond à la question"
+                + la_question
+                + "? Tu répondras vrai si la réponse est vraie et faux si elle est fausse, en argumentant.",
+            }
+        ],
     )
-    return vrai_ou_faux['choices'][0]['message']['content']
+    return vrai_ou_faux["choices"][0]["message"]["content"]
