54f334888f1e1fe477fbd3333e5f8a8117cd778c
Maxence Caucheteux <caucheteux.maxence@gmail.com>
2024-09-03 15:04:28 +0200

    commit1

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+import src.utils.ask_question_to_pdf
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/prompt', methods=['POST', 'GET'])
+def prompt():
+    if request.method == 'POST':
+        ans = src.utils.ask_question_to_pdf.gpt3_completion(request.form['prompt'])
+        print(ans)
+        s = jsonify({"answer": ans})
+        return s
