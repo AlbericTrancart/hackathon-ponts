b1e26859cb6fad3bb9d763d19771dfa909e6192e
Kenji Chikhaoui <kenji.chikhaoui@gmail.com>
2024-09-03 13:24:52 +0200

    yeet

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from flask import render_template
 from flask import Flask
 from flask import request
+from ask_question_to_pdf import (
+    gpt3_completion,
+    add_text_to_dialog,
+    initialise_discut,
+    input_question,
+    add_text_to_discussion,
+)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!<p>"
+    return """<p>Hello, World!<p> <a href="/hello"> click here <a>"""
 
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello(name=None):
+    initialise_discut()
     return render_template("index.html", name=name)
+
+
+@app.route("/question", methods=["GET"])
+def give_question():
+    input_question()
+    answer = gpt3_completion()
+    return {"answer": answer}
 
 
 @app.route("/prompt", methods=["POST"])
 def return_prompt():
-    return {"answer": request.form["prompt"]}
+    input = request.form["prompt"]
+    add_text_to_discussion(input)
+    answer = gpt3_completion()
+    return {"answer": answer}
