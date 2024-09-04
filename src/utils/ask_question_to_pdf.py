234d80abe1e517780571e7d5b5f759599074cfd9
Alexandre Tranié <alexandre.tranie@gmail.com>
2024-09-04 15:09:25 +0200

    Maj erreur typo

--- src/utils/ask_question_to_pdf.py
+++ src/utils/ask_question_to_pdf.py
@@ -8,14 +8,15 @@ from nltk.tokenize import sent_tokenize
 
 load_dotenv()
 
+
 def open_file(filepath):
     with open(filepath, "r", encoding="utf-8") as infile:
         return infile.read()
 
+
 client = OpenAI()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
-
 
 
 def read_pdf(filename):
@@ -73,24 +74,28 @@ chunks = split_text(document)
 
 
 context = "Tu es un assistant qui me pose des questions pertinentes sur les cours que je t'envoie de sorte à être exhaustif sur les notions abordées."
+
+
 def gpt3_completion():
     chunks_dic = []
     for t in chunks:
-        chunks_dic.append({"role":"user", "content": t})
+        chunks_dic.append({"role": "user", "content": t})
     msg = [{"role": "system", "content": context}]
     msg += chunks_dic
     msg += [
-        {"role": "user", "content": "Pose moi une question assez précise sur ce texte et dis moi si la réponse est bien correcte en considérant les informations du texte."},
+        {
+            "role": "user",
+            "content": "Pose moi une question assez précise sur ce texte et dis moi si la réponse est bien correcte en considérant les informations du texte.",
+        },
         {"role": "assistant", "content": "Quel était l'objectif initial de l'école ?"},
-        {"role": "user", "content": " L'objectif initial de l'école était de former des ingénieurs capables de concevoir et de superviser la construction de routes et de ponts pour améliorer l'infrastructure en France."},
+        {
+            "role": "user",
+            "content": " L'objectif initial de l'école était de former des ingénieurs capables de concevoir et de superviser la construction de routes et de ponts pour améliorer l'infrastructure en France.",
+        },
         {"role": "assistant", "content": "La réponse est correcte."},
-        {"role": "user", "content": "Pose moi une nouvelle question sur le texte."}
+        {"role": "user", "content": "Pose moi une nouvelle question sur le texte."},
     ]
-    response = client.chat.completions.create(
-        model = "gpt-3.5-turbo",
-        messages = msg
-    )
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=msg)
 
     message = response.choices[0].message.content
     return message
-
