1a4d3a11fb7b0b155590b3c60dcd316f05fef30f
Maxence Caucheteux <caucheteux.maxence@gmail.com>
2024-09-06 09:59:34 +0200

    c

--- static/darkModeToggle.js
+++ static/darkModeToggle.js
@@ -4,14 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
   // Load dark mode preference from localStorage if available
   if (localStorage.getItem('dark-mode') === 'enabled') {
     document.body.classList.add('dark-mode');
-    darkModeToggle.textContent = 'Toggle Light Mode'; // Set button text for dark mode
+    darkModeToggle.textContent = 'Light Mode'; // Set button text for dark mode
   } else {
-    darkModeToggle.textContent = 'Toggle Dark Mode'; // Set button text for light mode
+    darkModeToggle.textContent = 'Dark Mode'; // Set button text for light mode
   }
 
   darkModeToggle.addEventListener('click', () => {
     const isDarkMode = document.body.classList.toggle('dark-mode');
     localStorage.setItem('dark-mode', isDarkMode ? 'enabled' : 'disabled');
-    darkModeToggle.textContent = isDarkMode ? 'Toggle Light Mode' : 'Toggle Dark Mode';
+    darkModeToggle.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
   });
 });
